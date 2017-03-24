6e372a756d77459f451dd15f70d2b76af565c22c
wangzun <wangzun0009@gmail.com>
2017-03-24 17:01:12 +0800

    thrift

--- src/thrift.erl
+++ src/thrift.erl
@@ -7,20 +7,27 @@
 -include("thrift_constants.hrl").
 -include("thrift_protocol.hrl").
 
+-record(framed_transport, {wrapped, % a thrift_transport
+  read_buffer, % iolist()
+  write_buffer % iolist()
+}).
+
+-record(transport, {module = thrift_framed_transport, data = #framed_transport{}}).
+
+-record(binary_protocol, {transport= #transport{},
+  strict_read = true,
+  strict_write = true
+}).
+
+-record(protocol, {module, data}).
+%% module =  thrift_binary_protocol
+%% data =  #binary_protocol{}
+
+
 decode(Bin) ->
-	Proto0 = {protocol,
-		  thrift_binary_protocol,
-		  {binary_protocol,
-		   {transport, thrift_framed_transport,
-		    {framed_transport,
-		     {transport,
-		      thrift_socket_transport, {data, undefined, infinity}
-		     },
-		     Bin, []
-		    }
-		   }, false, true
-		  }
-		 },
+	{ok,Transport} = thrift_framed_transport:new(undefined),
+	{ok,Proto} = thrift_binary_protocol:new(Transport),
+	Proto0= Proto#protocol{data=#binary_protocol{transport= #transport{data = #framed_transport{read_buffer = Bin}}}},
 	{Proto1, MessageBegin} = thrift_protocol:read(Proto0, message_begin),
 	case MessageBegin of
 		{protocol_message_begin, Function, _, _Seqid} ->
@@ -33,19 +40,8 @@ decode(Bin) ->
 
 
 encode(Data) ->
-	Proto0 = {protocol,
-		  thrift_binary_protocol,
-		  {binary_protocol,
-		   {transport, thrift_framed_transport,
-		    {framed_transport,
-		     {transport,
-		      thrift_socket_transport, {data, undefined, infinity}
-		     },
-		     [], []
-		    }
-		   }, false, true
-		  }
-		 },
+	{ok,Transport} = thrift_framed_transport:new(undefined),
+	{ok,Proto0} = thrift_binary_protocol:new(Transport),
 	StructName = erlang:element(1, Data),
 	StructDef = simple_types:struct_info(StructName),
 	Begin = #protocol_message_begin{name = atom_to_list(StructName),
@@ -54,18 +50,6 @@ encode(Data) ->
 	{Proto1, ok} = thrift_protocol:write(Proto0, Begin),
 	{Proto2, ok} = thrift_protocol:write(Proto1, {StructDef, Data}),
 	{Proto3, ok} = thrift_protocol:write(Proto2, message_end),
-	{protocol,
-	 thrift_binary_protocol,
-	 {binary_protocol,
-	  {transport, thrift_framed_transport,
-	   {framed_transport,
-	    {transport,
-	     thrift_socket_transport, {data, undefined, infinity}
-	    },
-	    [], Bin
-	   }
-	  }, false, true
-	 }
-	} = Proto3,
+	#protocol{data=#binary_protocol{transport= #transport{data = #framed_transport{write_buffer= Bin}}}} =Proto3,
 	{ok, Bin}.
 
